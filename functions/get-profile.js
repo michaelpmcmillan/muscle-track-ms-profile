577487cc572c6185f1a0e930db8896f2db9a7d77
Michael.McMillan <michael.mcmillan@musclefood.com>
2020-10-20 16:06:42 +0100

    Add auth to function

--- functions/get-profile.js
+++ functions/get-profile.js
@@ -1,6 +1,20 @@
-exports.handler = async function(event, context) {
+function getProfile() {
     return {
-        statusCode: 200,
-        body: JSON.stringify({height: 100, date_of_birth: '1980-05-25'}),
+        height: 100, 
+        date_of_birth: '1980-05-25',
+    };
+}
+
+exports.handler = async function(event, context) {
+    const {identity, user} = context.clientContext;
+
+    console.log(JSON.stringify(user));
+
+    if (user) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(getProfile()),
+        };
     }
+    return { statusCode: 401 };
 }
